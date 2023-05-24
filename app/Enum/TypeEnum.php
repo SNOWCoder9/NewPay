94009c235bbfb11392d4b322c364cb2004332728
SNOWCoder9 <zhoushi1@pm.me>
2023-05-25 02:34:52 +0800

    wechat hk

--- app/Enum/TypeEnum.php
+++ app/Enum/TypeEnum.php
@@ -8,12 +8,14 @@ class TypeEnum
     const ALIPAY = 2;
     const WECHAT = 3;
     const ALIPAY_HK = 4;
+    const WECHAT_HK = 5;
 
     const type = [
         self::VIRTUAL => '虚拟货币',
         self::ALIPAY => '支付宝',
         self::WECHAT => '微信',
         self::ALIPAY_HK => '支付宝（香港）',
+        self::WECHAT_HK => '微信（香港）',
     ];
 
     public static function toType($payment)
@@ -25,6 +27,8 @@ class TypeEnum
                 return 3;
             case 'alipay_hk':
                 return 4;
+            case 'wechat_hk':
+                return 5;
             default:
                 return 1;
         }
